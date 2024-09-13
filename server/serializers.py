11f0de42c950dc0d99efa9931bd2733d73102449
David FB <david_flodin_bergqvist@live.se>
2024-09-13 12:05:19 +0200

    Add email + password validation

--- server/serializers.py
+++ server/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
 
 class UserSerializer(serializers.ModelSerializer):
+    # Use Djangos built-in email validation
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True, min_length=7)
+
     class Meta(object):
         model = User 
-        fields = ['id', 'username', 'password', 'email']
+        fields = ['id', 'email', 'password']
+
+
+    def validate_password(self, value):
+        # Use Django's built-in password validators
+        try:
+            validate_password(value)
+        except ValidationError as e:
+            # Customize the error message here
+            raise serializers.ValidationError({
+                'password': ' '.join(e.messages)  # Combines all error messages into a single string
+            })
+        return value
